4b15f9074396899dd27d78b0f72145f493ac8079
EmadBrown <emadbrown2013@gmail.com>
2018-01-25 18:57:15 +0100

    Add Api routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,21 @@ use Illuminate\Http\Request;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+// Buyer
+Route::resource('buyers' , 'Buyer\BuyerController' , ['only' => ['index', 'show']]);
+
+// Categories
+Route::resource('categories' , 'Category\CategoryController' , ['only' => ['create', 'edit']]);
+
+// Products
+Route::resource('products' , 'Product\ProductController' , ['only' => ['index', 'show']]);
+
+// Seller
+Route::resource('sellers' , 'Seller\SellerController' , ['only' => ['index', 'show']]);
+
+// Transactions
+Route::resource('transactions' , 'Transaction\TransactionController' , ['only' => ['index', 'show']]);
+
+// Users
+Route::resource('users' , 'User\UserController' , ['except' => ['create', 'edit']]);
