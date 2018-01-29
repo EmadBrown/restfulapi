60321b161bdfc2486c0a03f06116e7ad5ff1cb6a
EmadBrown <emadbrown2013@gmail.com>
2018-01-29 12:48:15 +0100

    Add Factory for Transaction in UserFactory file

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,8 +1,11 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\User;
+use App\Seller;
 use App\Product;
 use App\Category;
+use App\Transaction;
 
 /*
 |--------------------------------------------------------------------------
@@ -39,6 +42,19 @@ $factory->define(App\Product::class, function (Faker $faker) {
         'name' => $faker->word,
         'description' => $faker->paragraph(1),
         'quantity' => $faker->numberBetween(1, 10),
-        'verified' => $verified = $faker->randomElement([Product::AVAILABLE_PRODUCT , Product::UNAVAILABLE_PRODUCT]),
+        'status' => $faker->randomElement([Product::AVAILABLE_PRODUCT , Product::UNAVAILABLE_PRODUCT]),
+        'image' => $faker->randomElement(['1.jpg' , '2.jpg', '3.jpg']),
+        'seller_id' => User::all()->random()->id,
+        // User::inRandomOrder()->first->id,
+    ];
+});
+
+$factory->define(App\Transaction::class, function (Faker $faker) {
+  $seller = Seller::has('products')->get()->random();
+  $buyer = User::all()->except($seller->id)->random();
+    return [
+        'quantity' => $faker->numberBetween(1, 3),
+        'buyer_id' => $buyer->id,
+        'product_id' => $seller->products->random()->id,
     ];
 });
